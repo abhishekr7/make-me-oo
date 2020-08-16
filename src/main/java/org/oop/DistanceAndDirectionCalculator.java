1f8bd52c6c571395ed5f27d6d98095fcff5fb002
abhishekr7 <abhishekrajbhoj38@gmail.com>
2020-08-16 10:52:04 +0530

    Add changes

--- src/main/java/org/oop/DistanceAndDirectionCalculator.java
+++ src/main/java/org/oop/DistanceAndDirectionCalculator.java
@@ -2,14 +2,21 @@ package org.oop;
 
 public class DistanceAndDirectionCalculator {
     public static double distance(Point from, Point to) {
-        double xDistance = to.getX() - from.getX();
-        double yDistance = to.getY() - from.getY();
-        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+        //double xDistance = to.getX() - from.getX();
+        //double yDistance = to.getY() - from.getY();
+
+
+        double[] Distance = from.calculateDistance(to);
+
+        return Math.sqrt(Math.pow(Distance[0], 2) + Math.pow(Distance[1], 2));
     }
 
     public static double direction(Point from, Point to) {
-        double xDistance = to.getX() - from.getX();
-        double yDistance = to.getY() - from.getY();
-        return Math.atan2(yDistance, xDistance);
+        //double xDistance = to.getX() - from.getX();
+        //double yDistance = to.getY() - from.getY();
+
+        double[] Distance = from.calculateDistance(to);
+
+        return Math.atan2(Distance[1], Distance[0]);
     }
 }
